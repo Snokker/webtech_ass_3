d9d256c8955c1fa490f051c0a75e5d4807d5af66
Esther Bouma <esther.r.bouma@gmail.com>
2018-04-15 23:57:12 +0200

    aaa

--- FillProductDatabase.js
+++ FillProductDatabase.js
@@ -1,6 +1,5 @@
 ﻿//Function to fill a text file with insert queries. 
-window.onload()
-{
+
     var manufacturers = ['Femke', 'Margriet', 'FemkeEnMargriet', 'MargrietEnFemke', 'Margke', 'Femgriet', 'Greta', 'Gerda', 'China', 'John Snow'];
     var categories = ['Hobby', 'Hackerman', 'Non-Alcoholic', 'Juice', 'Yoga-pants', 'Avocado', 'Home-sweet-home products', 'Good shit', 'Medium shit', 'Only for the rich'];
     var title = ['Scissors', 'Box', 'Plastic Box', 'Hot Glue Pistol', 'Hot Glue',
@@ -9,20 +8,21 @@ window.onload()
         'Seven Cats', 'A dead rat', 'Led Lights', 'Definitely not drugs', 'Google?',
         'Tote Bag', 'Awkward turtle', 'Shooting star', 'Kazoo', 'Saxophone',
         'Clown suit', 'Yoga Mat', 'Piggy Bank(money not included)', 'Emo lipstick', 'Tomato'];
+    fillDB();
 
-}
 
 function fillDB()
 {
 
     text = '';
-    for (var i = 0; i < title.length;) {
+    for (var i = 0; i < title.length;i++) {
         text += 'INSERT INTO PRODUCT VALUES('
             + title[chooseRandomIndex(1, title.length)] + ','
             + manufacturers[chooseRandomIndex(1, manufacturers.length)] + ','
             + categories[chooseRandomIndex(1, categories.length)] + ','
-            + chooseRandom(2, 2000) + "';";
+            + chooseRandom(2, 2000) + "';" + "\n";
     }
+    console.log(text);
 }
 
 function chooseRandom(min, max)
@@ -30,7 +30,14 @@ function chooseRandom(min, max)
     return Math.random() * (max - min + 1) + min;
 }
 
-function chooseRandomIndex()
+function chooseRandomIndex(min, max)
 {
-    return Math.floor(Math.random() * items.length);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function WriteToFile() {
+    var txt = new ActiveXObject("Scripting.FileSystemObject");
+    var s = txt.CreateTextFile("11.txt", true);
+    s.WriteLine(text);
+    s.Close();
 }
